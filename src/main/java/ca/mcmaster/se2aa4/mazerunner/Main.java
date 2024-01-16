e4042038da8f500e448acbc7f3df5e252e51b5a5
Matthew Nesbitt <nesbim2@mcmaster.ca>
2024-01-16 16:03:28 -0500

    Setup Configuration

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,6 +3,9 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.ObjectInputFilter.Config;
+import java.lang.module.Configuration;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.CommandLine;
@@ -17,14 +20,10 @@ public class Main {
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
-        Options options = new Options();
-        options.addOption("i", true, "i flag");
-        CommandLineParser parser = new DefaultParser();
         try {
-            CommandLine cmd = parser.parse(options, args);
-            String input_maze  = cmd.getOptionValue("i");
-            logger.info("**** Reading the maze from file " + input_maze);
-            BufferedReader reader = new BufferedReader(new FileReader(input_maze));
+            Configuration config = configure(args);
+            logger.info("**** Reading the maze from file " + config.input_maze());
+            BufferedReader reader = new BufferedReader(new FileReader(config.input_maze()));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -34,13 +33,25 @@ public class Main {
                         System.out.println("PASS ");
                     }
                 }
-                logger.info(System.lineSeparator());
+                System.out.println(System.lineSeparator());
             }
         } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\");
+            logger.error(e.getMessage());
+            System.exit(1);
         }
         logger.info("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
         logger.info("** End of MazeRunner");
     }
+
+    private static Configuration configure(String[] args) throws ParseException{
+        Options options = new Options();
+        options.addOption("i", true, "i flag");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        String maze  = cmd.getOptionValue("i");
+        return new Configuration(maze);
+    }
+
+    private record Configuration(String input_maze) {}
 }
