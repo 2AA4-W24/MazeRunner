d3cfd164b4e8b65549c90dd3b62bb230aa2d7ba4
Matthew Nesbitt <nesbim2@mcmaster.ca>
2024-01-16 20:37:12 -0500

    Finished updates to skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgo.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgo.java
@@ -4,6 +4,7 @@ public class RightHandAlgo implements MazeSolver{
     
     @Override
     public String solveMaze(Maze maze) {
+        MazeRunner runner = new MazeRunner(maze);
         return "No solution yet :(";
     }
 }
